f3d421af5a6d5c735d80bbf0ae9440148f2e1660
kardy04 <kardy04@postech.ac.kr>
2016-11-05 16:18:21 +0900

    Test case update (accpet/reject case) except check pending

--- test/edu/ncsu/csc/itrust/uc92/UC92SeleniumTest.java
+++ test/edu/ncsu/csc/itrust/uc92/UC92SeleniumTest.java
@@ -37,7 +37,7 @@ public class UC92SeleniumTest  extends iTrustSeleniumTest{
 		// fill form
 		Select select;
 		select = new Select (driver.findElement(By.name("apptType")));
-		select.selectByValue("Physical Therapy");
+		select.selectByValue("Orthopedic");
 		select = new Select (driver.findElement(By.name("time1")));
 		select.selectByValue("11");
 		select = new Select (driver.findElement(By.name("time2")));
@@ -45,7 +45,7 @@ public class UC92SeleniumTest  extends iTrustSeleniumTest{
 		select = new Select (driver.findElement(By.name("time3")));
 		select.selectByValue("AM");
 		select = new Select (driver.findElement(By.name("lhcp")));
-		select.selectByValue("9210000000");
+		select.selectByValue("9220000000");
 		element = driver.findElement(By.name("startDate"));
 		element.clear();
 		element.sendKeys("1/12/2016");
@@ -63,7 +63,7 @@ public class UC92SeleniumTest  extends iTrustSeleniumTest{
 		
 		//log-out and log-in as physical therapist
 		driver.close();
-		driver = login("9210000000", "pw");
+		driver = login("9220000000", "pw");
 		assertEquals("iTrust - HCP Home", driver.getTitle());
 		
 		//find element appointment request
@@ -76,26 +76,92 @@ public class UC92SeleniumTest  extends iTrustSeleniumTest{
 		
 		//Accept request
 		driver.findElement(By.linkText("Approve")).click();
-		
-			//The appointment request you selected has been accepted and scheduled.
+		assertTrue(driver.findElement(By.xpath("//*[@id='iTrustContent']")).getText().contains
+				("The appointment request you selected has been accepted and scheduled."));
 		
 		//Check appointment
 		element = driver.findElement(By.linkText("View My Appointments"));
+		assertTrue(driver.findElement(By.xpath("//*[@id='iTrustContent']")).getText().contains("Random Person"));
 		
 		//logout and login as patient
 		driver.close();
 		driver = login("1", "pw"); 
 		assertEquals("iTrust - Patient Home", driver.getTitle());
 		
-		//check request result
+		//check request result from appointment page
 		element = driver.findElement(By.linkText("View My Appointments"));
 		element.click();
+		assertTrue(driver.findElement(By.xpath("//*[@id='iTrustContent']")).getText().contains("Momsen doctor"));
 		
-		assertTrue(driver.findElement(By.xpath("//*[@id='iTrustContent']")).getText().contains("Taylor Physical Therapist"));
+		//check request result from request view page
+		element = driver.findElement(By.linkText("View My Requests"));
+			//Not yet implemented
+		
 	}
 	
 	public void testReqeustAndReject() throws Exception {
-		
+		// login as "Random Person"
+				driver = login("26", "pw"); 
+				assertEquals("iTrust - Patient Home", driver.getTitle());
+				
+				// find element of Appointment Requests
+				element = driver.findElement(By.linkText("Appointment Requests"));
+				element.click();
+				assertEquals("iTrust - Appointment Requests", driver.getTitle());
+				
+				// fill form
+				Select select;
+				select = new Select (driver.findElement(By.name("apptType")));
+				select.selectByValue("Orthopedic");
+				select = new Select (driver.findElement(By.name("time1")));
+				select.selectByValue("1");
+				select = new Select (driver.findElement(By.name("time2")));
+				select.selectByValue("00");
+				select = new Select (driver.findElement(By.name("time3")));
+				select.selectByValue("PM");
+				select = new Select (driver.findElement(By.name("lhcp")));
+				select.selectByValue("9220000000");
+				element = driver.findElement(By.name("startDate"));
+				element.clear();
+				element.sendKeys("1/12/2016");
+				element.submit();
+				
+				assertEquals("iTrust - Appointment Requests", driver.getTitle());
+				assertTrue(driver.findElement(By.xpath("//*[@id='iTrustContent']")).getText().contains("Your appointment request has been saved and is pending."));
+				
+				//find element of View My Requests
+				element = driver.findElement(By.linkText("View My Requests"));
+				element.click();
+				assertEquals("iTrust - View My Requests", driver.getTitle());
+				//find there exists new request with status is pending
+					//Not Implemented of Interface of View My Requests
+				
+				//log-out and log-in as physical therapist
+				driver.close();
+				driver = login("9220000000", "pw");
+				assertEquals("iTrust - HCP Home", driver.getTitle());
+				
+				//find element appointment request
+				element = driver.findElement(By.linkText("Appointment Requests"));
+				element.click();
+				assertEquals("iTrust - View My Appointment Requests", driver.getTitle());
+				
+				//check there exists request from random person
+				assertTrue(driver.findElement(By.xpath("//*[@id='iTrustContent']")).getText().contains("Request from: Random Person"));
+				
+				//Accept request
+				driver.findElement(By.linkText("Reject")).click();
+				assertTrue(driver.findElement(By.xpath("//*[@id='iTrustContent']")).getText().contains
+						("The appointment request you selected has been rejected."));
+				
+				//logout and login as patient
+				driver.close();
+				driver = login("26", "pw"); 
+				assertEquals("iTrust - Patient Home", driver.getTitle());
+				
+				//check request result from request view page
+				element = driver.findElement(By.linkText("View My Requests"));
+					//Not yet implemented
 	}
 	
 }
