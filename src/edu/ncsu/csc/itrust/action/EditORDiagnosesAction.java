5181284d3b2f4971abf40c4af12fae6fd791da31
todok <todok@DESKTOP-14PCL2D>
2016-11-12 21:55:28 +0900

    Add EditORDiagnosesAction With sinjint

---
+++ src/edu/ncsu/csc/itrust/action/EditORDiagnosesAction.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package edu.ncsu.csc.itrust.action;
+
+import java.util.List;
+import edu.ncsu.csc.itrust.beans.OrthopedicDiagnosisBean;
+import edu.ncsu.csc.itrust.dao.DAOFactory;
+import edu.ncsu.csc.itrust.dao.mysql.OrthopedicDiagnosisDAO;
+import edu.ncsu.csc.itrust.exception.DBException;
+import edu.ncsu.csc.itrust.exception.ITrustException;
+
+/**
+ * Handle patients Diagnosis
+ * Edit Diagnosis
+ * Add Diagnosis
+ * Remove Diagnosis
+ *  
+ *  
+ */
+public class EditORDiagnosesAction {
+	
+	private OrthopedicDiagnosisDAO diagnosesDAO;
+	private String officeVisitID;
+	
+	public EditORDiagnosesAction(DAOFactory factory, String ovIDString ) 
+		throws ITrustException {
+		diagnosesDAO = factory.getORDiagnosisDAO();
+		officeVisitID=ovIDString;
+		
+	}
+	
+	public List<OrthopedicDiagnosisBean> getDiagnoses() throws DBException {
+			return diagnosesDAO.getList(Integer.parseInt(officeVisitID));
+	}
+	
+	public void addDiagnosis(OrthopedicDiagnosisBean bean) throws ITrustException {
+		diagnosesDAO.add(bean);
+	}
+	
+	public void editDiagnosis(OrthopedicDiagnosisBean bean) throws ITrustException {
+		diagnosesDAO.edit(bean);
+		
+	}
+	
+	public void deleteDiagnosis(OrthopedicDiagnosisBean bean) throws ITrustException {
+		diagnosesDAO.remove(bean.getOrDiagnosisID());
+		
+	}
+	
+	public List<OrthopedicDiagnosisBean> getDiagnosisCodes() throws DBException {
+		return diagnosesDAO.getOrICDCodes();
+	}
+	
+}
