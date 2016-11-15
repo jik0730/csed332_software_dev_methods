36ae4eab6560fa24f3dec4f169706cc3b6c97739
DoHyeok Kim <dohyeokkim@postech.ac.kr>
2016-11-15 19:15:45 +0900

    Add ImageDataServlet

--- src/edu/ncsu/csc/itrust/server/ImageDataServlet.java
+++ src/edu/ncsu/csc/itrust/server/ImageDataServlet.java
@@ -2,6 +2,8 @@ package edu.ncsu.csc.itrust.server;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -23,22 +25,36 @@ public class ImageDataServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// /Orthopedic/MRI/wow/oid/3
-		List<String> paths = Arrays.asList(req.getPathInfo().split("/"));
+		// ?table=orthopedic&col=MRI&pri=oid&key=3
+		InputStream in = null;
+		OutputStream out = null;
 		try {
-			if (paths.size() != 3) throw new NoSuchElementException();
-			String tableName = paths.get(0);
-			String colName = paths.get(1);
-			String idName = paths.get(2);
-			String primaryKey = paths.get(3);
+			String tableName = req.getParameter("table");
+			String colName = req.getParameter("col");
+			String idName = req.getParameter("pri");
+			String primaryKey = req.getParameter("key");
 
-			InputStream in = getImageStream(tableName, colName, idName, primaryKey);
+			if(tableName == null || colName == null || idName == null | primaryKey == null)
+				throw new NoSuchElementException();
+			
+			in = getImageStream(tableName, colName, idName, primaryKey);
 			
 			resp.setContentType("image");
-			//TODO: Write Stream To Response
-		} catch (NoSuchElementException e) {
+			out = resp.getOutputStream();
+			byte[] buf = new byte[512];
+			int read = 0;
+			while((read = in.read(buf))>0) {
+				out.write(buf, 0, read);;
+			}
+		} catch (IOException | NoSuchElementException e) {
 			resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			e.printStackTrace();
 			return;
+		} finally {
+			if (in != null)
+				in.close();
+			if (out != null)
+				out.close();
 		}
 	}
 	
@@ -48,12 +64,11 @@ public class ImageDataServlet extends HttpServlet{
 		DAOFactory factory = DAOFactory.getProductionInstance();
 		try {
 			conn = factory.getConnection();
-			ps = conn.prepareStatement("SELECT ? FROM ? WHERE ?=?");
-			ps.setString(1, colName);
-			ps.setString(2, tableName);
-			ps.setString(3, idName);
-			ps.setString(4, primaryKey);
+			/** TODO: Serious Security Problem: SQL Injection */
+			ps = conn.prepareStatement("SELECT "+ colName +" FROM "+ tableName +" WHERE "+ idName +"=?");
+			ps.setInt(1, Integer.valueOf(primaryKey));
 			ResultSet rs = ps.executeQuery();
+			rs.next();
 
 			InputStream in = rs.getBinaryStream(colName);
 			rs.close();
@@ -61,6 +76,7 @@ public class ImageDataServlet extends HttpServlet{
 
 			return in;
 		} catch (SQLException e) {
+			e.printStackTrace();
 			throw new NoSuchElementException();
 		} finally {
 			DBUtil.closeConnection(conn, ps);
